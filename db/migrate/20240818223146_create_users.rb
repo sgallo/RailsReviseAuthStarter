51b45ea5b74c0495deddb9630fca82c2c7b3e961
Stephen Gallo <sgallo1@gmail.com>
2024-08-18 18:38:06 -0400

    First commit

---
+++ db/migrate/20240818223146_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.2]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.datetime :confirmed_at
+      t.string :unconfirmed_email
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
