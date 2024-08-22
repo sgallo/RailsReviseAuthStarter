7dae50eb7f80cad4f382bc6a13648d0fd6544ed8
Stephen Gallo <sgallo1@gmail.com>
2024-08-21 21:54:42 -0400

    Fixed Logout by adding button to

--- config/initializers/revise_auth.rb
+++ config/initializers/revise_auth.rb
@@ -2,4 +2,5 @@ ReviseAuth.configure do |config|
   # Customize the params for registration and update profile.
   # config.sign_up_params += [:time_zone]
   # config.update_params += [:time_zone]
+  config.minimum_password_length = 5
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   revise_auth
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+
   # Defines the root path route ("/")
-  # root "posts#index"
+   root "home#index"
 end
