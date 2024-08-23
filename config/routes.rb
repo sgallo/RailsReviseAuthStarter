6cf1a39421180377a94da540c2c49e1d52de83b5
Stephen Gallo <sgallo1@gmail.com>
2024-08-22 21:16:05 -0400

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-
   # Defines the root path route ("/")
-   root "home#index"
+  root "home#index"
 end
